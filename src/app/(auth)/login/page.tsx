5895d31d90736f62dd3357b5014356efcb612da3
Priyanshu Jayswal <220020047@iitdh.ac.in>
2025-09-26 18:01:02 +0000

    why after clicking on create post , form is not openign to create a post

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,5 @@
 import { AuthForm } from '@/components/auth-forms';
-import { getCurrentUser } from '@/lib/session';
-import { redirect } from 'next/navigation';
 
 export default async function LoginPage() {
-  const user = await getCurrentUser();
-  if (user) {
-    redirect('/');
-  }
-
   return <AuthForm />;
 }
